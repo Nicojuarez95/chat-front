b0afb65be7919b2b5099a6633e4c8cfe846ce517
Nico <juareznico61@gmail.com>
2023-03-27 19:45:50 -0300

    asdasd

--- src/AuthPage.jsx
+++ src/AuthPage.jsx
@@ -5,7 +5,7 @@ const AuthPage = (props) => {
       e.preventDefault();
       const { value } = e.target[0];
       axios.post(
-        "http://localhost:8000/authenticate",
+        "https://chat-e3fi.onrender.com/authenticate",
         {username: value}
       )
       .then(r => props.onAuth({ ...r.data, secret: value }))
